9f3658a29bda2cbe4143e5f778e42fc8cb87da74
Festanny <junior-format30@mail.ru>
2022-06-01 22:46:40 +0400

    upd

--- js/script.js
+++ js/script.js
@@ -6,9 +6,24 @@ window.onload = function () {
 		document.body.classList.remove('loaded_hiding');
 	}, 500);
 	document.body.style.overflow = 'auto';
+	mainBlock();
+	percent();
+	imgWomen();
+}
 
+function percent() {
 	let number_1 = $('#token .info-block .text-main .progress .line .numbers span:nth-of-type(1)').text().replace(/\./g,''),
-		number_2 = $('#token .info-block .text-main .progress .line .numbers span:nth-of-type(2)').text().replace(/\./g,''),
-		percent = (number_1/number_2) * 100;
-	document.documentElement.style.setProperty("--test", percent + "%");
+	number_2 = $('#token .info-block .text-main .progress .line .numbers span:nth-of-type(2)').text().replace(/\./g,''),
+	percent = (number_1/number_2) * 100;
+	document.documentElement.style.setProperty("--progress", percent + "%");
+}
+
+function mainBlock() {
+	let heightHeader = $('header').height();
+	$('main').css('margin-top', -heightHeader);
+}
+
+function imgWomen() {
+	let bottomWomen = (($('#token').height() - $('#token .container').height())/2)-50;
+	$('.women').css('bottom', -bottomWomen);
 }
